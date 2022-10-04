471bac231a8ecb917be20c696969686f1526de1f
Padillaguadalupe <Padillaguadalupe.epet20@gmail.com>
2022-10-03 22:06:20 -0300

    Subiendo codigo JS

---
+++ script.js
@@ -0,0 +1,46 @@
+script.js
+
+
+//#region 
+var num1= 1;
+let numA= 2;
+var suma = 0;
+
+var num1= 1;
+let numB= 2;
+var resta= 0;
+
+
+var num1= 1;
+let numC= 2;
+var multiplicacion = 0;
+
+var num1= 1;
+let numD= 2;
+var division = 0;
+//#endregion
+
+//#region 
+suma= num1+numA;
+resta= num1+numB;
+multiplicacion= num1*numC;
+division= num1/numD;
+//#endregion
+
+//#region 
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+
+//#endregion
+
+//#region 
+var inicio= 0;
+var fin= [10];
+
+for(inicio of fin){
+    console.log(inicio++)
+}
+
+//#endregion
